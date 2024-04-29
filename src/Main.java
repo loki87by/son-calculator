488a101a1fd0eb9a1fc24de08dae5af22e19aa69
loki87by <loki87.666@gmail.com>
2024-04-29 17:57:02 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double res;
+        double second = 0;
+        boolean fin = false;
+        System.out.print("Введите первое число: ");
+        res = scan.nextDouble();
+        while (!fin) {
+            System.out.print("Введите операцию (+, -, *, /, =): ");
+            char operator = scan.next().charAt(0);
+            if (operator != '=') {
+                System.out.print("Введите следующее число: ");
+                second = scan.nextDouble();
+            }
+            switch (operator) {
+                case '+':
+                    res += second;
+                    break;
+                case '-':
+                    res -= second;
+                    break;
+                case '*':
+                    res *= second;
+                    break;
+                case '/':
+                    res /= second;
+                    break;
+                case '=':
+                    fin = true;
+                    break;
+                default:
+                    System.out.println("Ошибка ввода: операии "+operator+" не существует.");
+            }
+            System.out.println("Результат: " + res);
+        }
+    }
+}
