92bbb45539492e112b0123de980001033de13e26
loki87by <loki87.666@gmail.com>
2024-04-30 16:30:40 +0300

    docs: add comments

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double rez;
-        double second = 0;
-        boolean fin = false;
-        char znak;
-        System.out.print("Введите первое число: ");
-        rez = scan.nextDouble();
-        while (!fin) {
-            System.out.print("Введите операцию (+, -, *, /, =): ");
-            znak = scan.next().charAt(0);
-            if (znak != '=') {
+        double rez; // Объявляем переменную для хранения результата вычислений
+        double second = 0; // Объявляем переменную для хранения второго числа
+        boolean fin = false; // Объявляем переменную для проверки завершения вычислений
+        char znak; // Объявляем переменную для хранения операции
+
+        System.out.print("Введите первое число: "); // Просим пользователя ввести первое число
+        rez = scan.nextDouble(); // Считываем введенное число и сохраняем его как результат
+
+        while (!fin) { // Запускаем цикл, пока не будет введен знак "="
+            System.out.print("Введите операцию (+, -, *, /, =): "); // Просим пользователя ввести операцию
+            znak = scan.next().charAt(0); // Считываем введенный символ и сохраняем его в операцию
+
+            if (znak != '=') { // Если операция не равна "=", запрашиваем второе число для операции
                 System.out.print("Введите следующее число: ");
-                second = scan.nextDouble();
+                second = scan.nextDouble(); // Считываем второе число и сохраняем его
             }
-            switch (znak) {
-                case '+':
-                    rez += second;
+
+            switch (znak) { // Проверяем значение операции
+                case '+': // Если операция - сложение
+                    rez += second; // Прибавляем второе число к результату
                     break;
-                case '-':
-                    rez -= second;
+                case '-': // Если операция - вычитание
+                    rez -= second; // Выполняем вычитание второго числа из результата
                     break;
-                case '*':
-                    rez *= second;
+                case '*': // Если операция - умножение
+                    rez *= second; // Умножаем результат на второе число
                     break;
-                case '/':
-                    rez /= second;
+                case '/': // Если операция - деление
+                    rez /= second; // Делим результат на второе число
                     break;
-                case '=':
-                    fin = true;
+                case '=': // Если операция - равенство
+                    fin = true; // Завершаем работу программы
                     break;
-                default:
-                    System.out.println("Ошибка ввода: операции " + znak + " не существует.");
+                default: // Если введено что-то другое
+                    System.out.println("Ошибка ввода: операции " + znak + " не существует."); // Выводим сообщение об ошибке
             }
-            System.out.println("Результат: " + rez);
+            System.out.println("Результат: " + rez); // Выводим результат вычислений
         }
     }
 }
